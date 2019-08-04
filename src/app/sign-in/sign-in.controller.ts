3cf87092d356e9b61ce73c75e9dd319b169e497f
IlyaMatvienko <ilya.matvienko@binomo.com>
2019-08-04 21:10:27 +0300

    fix path

--- src/app/sign-in/sign-in.controller.ts
+++ src/app/sign-in/sign-in.controller.ts
@@ -2,7 +2,7 @@ import { Component, NgZone, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { watch } from '../../modules/firewatch/firewatch';
+import { watch } from 'modules/firewatch';
 
 declare const FB;
 
